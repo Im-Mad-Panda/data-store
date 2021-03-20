1bd2dc28b0fc9d23bf78564252e5e74e6921b904
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-03-21 01:15:50 +0200

    footer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-const tetSlider = new Swiper('.test-slider',{
-    slidesPerView: 3,
+const tetSlider = new Swiper('.test-slider', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.arrow-left',
+    prevEl: '.arrow-right',
+  },
+  pagination: {
+    el: '.slider-pagination',
+    bulletClass: 'slider-bullet',
+    bulletActiveClass: 'slider-active-bullet'
+  },
     navigation: {
-        nextEl: '.arrow-left',
-        prevEl: '.arrow-right',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-      },
-    
-      
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
 })
